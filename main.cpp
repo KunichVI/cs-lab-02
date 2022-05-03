75047418d6464ac01b4ad946e4e4a0bf6db400f0
Bob(KunichVI) <bob@example.com>
2022-05-04 02:09:06 +0300

    code: добавлен вывод произведение чисел

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main()
     int a, b;
     cin >> a >> b;
     cout << "a + b = "<< a + b <<'\n'
-         << "a - b = "<< a - b << '\n';
+         << "a - b = "<< a - b << '\n'
+         << "a * b = "<< a * b << '\n';
 }
