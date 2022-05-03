6b5a558430bf7bec9fc4776b708c1395eec00799
Alice(KunichVI) <alice@example.com>
2022-05-04 02:16:08 +0300

    code: добавлен вывод максимума

--- main.cpp
+++ main.cpp
@@ -6,9 +6,12 @@ int main()
 {
     cout << "Enter A and B: ";
     int a, b;
+    int max = a;
+    if (b > a) max = b;
     cin >> a >> b;
     cout << "a + b = "<< a + b <<'\n'
          << "a - b = "<< a - b << '\n'
          << "a * b = "<< a * b << '\n'
-         << "a / b = "<< a / b << '\n';
+         << "a / b = "<< a / b << '\n'
+         <<"max in numbers:" << max <<'\n';
 }
